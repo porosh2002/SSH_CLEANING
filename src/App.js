1c89c63ab37bc65720092a81fabe183bb685fdf3
porosh <jamilkashem2002@gmail.com>
2021-01-11 01:17:04 +0600

    CTA complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navigation from './Components/Navigation/Navigation'
 import './App.css'
+import CTA from './Components/CTA/CTA'
 function App() {
   return (
     <div>
       <Navigation />
+      <CTA />
     </div>
   )
 }
