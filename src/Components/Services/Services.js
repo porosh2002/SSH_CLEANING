794e25e6d50c1bf1d0e30de6f00720946aae4081
porosh <jamilkashem2002@gmail.com>
2021-01-11 02:11:53 +0600

    Services

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -5,7 +5,21 @@ function Services() {
     return (
         <div className='Services'>
             <p className='Section_Title'>Our Services</p>
-            <ServiceCard ServiceClass='ServiceCard Service_1' name={'Air Hygiene & Ductwork System Cleaning'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_1' name={'Air Hygiene & Ductwork  Cleaning'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_2' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_3' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_4' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_5' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_6' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_7' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_8' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_9' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_10' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_11' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_12' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_13' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_14' name={'Builders And Handover Cleans'}/>
+            <ServiceCard ServiceClass='ServiceCard Service_15' name={'Builders And Handover Cleans'}/>
         </div>
     )
 }
