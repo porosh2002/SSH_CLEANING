2d771f120cbaf74905b9049bf0cd888d22cceadb
porosh <jamilkashem2002@gmail.com>
2021-01-11 01:26:13 +0600

    Services section with Section Title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Navigation from './Components/Navigation/Navigation'
 import './App.css'
 import CTA from './Components/CTA/CTA'
+import Services from './Components/Services/Services'
 function App() {
   return (
     <div>
       <Navigation />
       <CTA />
+      <Services />
     </div>
   )
 }
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div className='Services'>
+            <p className='Section_Title'>Our Services</p>
+        </div>
+    )
+}
+
+export default Services
