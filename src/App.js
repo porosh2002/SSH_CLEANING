6c64420310b8567cccd34268c9af9a440ceb67e3
porosh <jamilkashem2002@gmail.com>
2021-01-12 01:18:41 +0600

    Last Cta 2 added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navigation from './Components/Navigation/Navigation'
 import './App.css'
 import CTA from './Components/CTA/CTA'
-// import CTA2 from './Components/CTA2/CTA'
+import CTA2 from './Components/CTA2/CTA'
 import Services from './Components/Services/Services'
 import RecentWork from './Components/RecentWork/RecentWork'
 import News from './Components/News/News'
@@ -16,7 +16,7 @@ function App() {
       <RecentWork  />
       <News />
       <Clients />
-      {/* <CTA2 /> */}
+      <CTA2 />
     </div>
   )
 }
--- src/Components/CTA2/CTA.js
+++ src/Components/CTA2/CTA.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function CTA() {
     return (
-        <div className='CTA'>
+        <div className='CTA2'>
             <div className='CTA_Wrap'>
                 <div className='CTA_Text'>
                     <p className='CTA_Title'>
-                We are one of the UK's leading specialist cleaning suppliers</p>
+                    Our work does the talking</p>
 
                     <Link className='CTA_BTN_IN' to='#'>Contact us</Link>
                 </div>
